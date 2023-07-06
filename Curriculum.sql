3168003e838398e49c2344fea08d6fca5df4aa63
ranisaaulia <ranisaaulia31@gmail.com>
2023-07-06 15:39:41 +0700

    insert data and update

--- Curriculum.sql
+++ Curriculum.sql
@@ -1,5 +1,9 @@
 create schema curriculum
 
+create schema master
+
+create schema users
+
 create table curriculum.program_entity_description(
 	pred_prog_entity_id integer primary key,
 	pred_item_learning varchar,
@@ -9,6 +13,57 @@ create table curriculum.program_entity_description(
 	pred_target_level varchar
 )
 
+insert into curriculum.program_entity_description (pred_prog_entity_id, pred_item_learning, pred_item_include, pred_requirement, pred_description, pred_target_level) values
+(1, 'Programming fundamentals', 'Yes', 'None', 'This module covers the basics of programming, including variables, control flow, and data structures.', 'Beginner'),
+(2, 'Database management', 'Yes', 'Basic knowledge of SQL', 'This module focuses on database management, including designing schemas, writing queries, and optimizing performance.', 'Intermediate'),
+(3, 'Web development', 'No', 'Front-end development experience', 'This module delves into web development, covering HTML, CSS, JavaScript, and frameworks like React or Angular.', 'Advanced')
+
+
+select * from curriculum.program_entity_description
+
+create table master.city(
+	city_id serial primary key
+)
+
+insert into master.city (city_id) values
+(1),
+(2)
+
+
+create table master.category(
+	cate_id serial primary key
+)
+
+insert into master.category (cate_id) values
+(2),
+(3),
+(4)
+
+create table master.status(
+	status varchar(15) primary key
+)
+
+insert into master.status (status) values
+('publish')
+
+create table users.users(
+	created_by varchar(20) unique,
+	user_entity_id integer unique,
+	foreign key (user_entity_id) references users.business_entity (entity_id),
+	constraint user_entity_id primary key (user_entity_id, created_by)
+)
+
+insert into users.users (created_by,user_entity_id) values
+('kangdian', 1),
+('nofal', 2),
+('abdul101', 3),
+('ratih', 4),
+('eka', 5),
+('novia', 6),
+('novelina', 7),
+('yugo', 8),
+('andhika', 9)
+
 create table curriculum.program_entity(
 	prog_entity_id serial primary key,
 	prog_title varchar(256),
@@ -17,24 +72,30 @@ create table curriculum.program_entity(
 	prog_learning_type varchar(15) CHECK (prog_learning_type IN ('online', 'offline','both')),
 	prog_rating integer,
 	prog_total_trainee integer,
-	prog_modified_date timestamp,
 	prog_image varchar(256),
-	prog_best_seller char(1) CHECK (prog_best_seller in (0, 1)),
+	prog_best_seller char(1) CHECK (prog_best_seller in ('0', '1')),
 	prog_price integer,
 	prog_language varchar(35),
 	prog_modified_date timestamp,
 	prog_duration integer,
 	prog_duration_type varchar(15) CHECK (prog_duration_type in ('month', 'week','days')),
 	prog_tag_skill varchar(512),
-	prog_city_id integer,
-	prog_cate_id integer,
+	prog_city_id integer unique,
+	prog_cate_id integer unique,
 	prog_created_by integer,
-	prog_status varchar(15) CHECK (prog_status in('draft','publish'))
+	prog_status varchar(15) CHECK (prog_status in('draft','publish')),
 	foreign key (prog_city_id) references master.city (city_id),
 	foreign key (prog_cate_id) references master.category (cate_id),
 	foreign key (prog_created_by) references users.users (created_by),
 	foreign key (prog_status) references master.status (status)								
 )
+
+insert into curriculum.program_entity (prog_entity_id, prog_title, prog_headline, prog_type, prog_learning_type, prog_rating, prog_total_trainee, prog_image, prog_best_seller, prog_price, prog_language, prog_modified_date, prog_duration, prog_duration_type, prog_tag_skill, prog_city_id, prog_cate_id, prog_created_by, prog_status) values
+(1, 'Javascript Fullstack From Zero To Expert', 'Modern javascript for everyone with projects, challenge and theory. More Course in one !', 'course', 'online', null, null, 'javascript.png', null, 750.000, 'bahasa', null, null, null, null, 1, 2, null, 'publish' ),
+(2, 'Java Technology', 'Join bootcamp and become java developer', 'bootcamp', 'online', null, null, 'java.png', null, null, null, null, null, null, null, 2, 3, null, null ),
+(3, 'Golang', 'Develop application with golang', 'course', 'online', null, null, null, null, null, null, null, null, null, null, null, 4, null, null )
+
+select * from curriculum.program_entity
 	
 -- CREATE SEQUENCE prog_entity_id TABLE PROGRAM ENTITY
 CREATE SEQUENCE seq_prog_entity_id
@@ -51,17 +112,40 @@ language sql
 
 alter table curriculum.program_entity alter column prog_entity_id set default progEntityIdSeq()
 
+create table users.business_entity(
+	entity_id serial primary key
+)
+
+insert into users.business_entity (entity_id) values
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9)
+
 create table curriculum.program_reviews(
 	prow_user_entity_id integer,
 	prow_prog_entity_id integer,
 	prow_review varchar(512),
 	prow_rating integer,
 	prow_modified_date timestamp,
-	constraint prow_user_entity_id primary key (prow_user_entity_id, prow_prog_entity_id),
-	foreign key (prow_user_entity_id) references master.users (user_entity_id),
-	foreign key (prow_prog_entity_id) references curriculum.program_entity (prog_entity_id)	
+	foreign key (prow_user_entity_id) references users.users (user_entity_id),
+	foreign key (prow_prog_entity_id) references curriculum.program_entity (prog_entity_id),
+	constraint prow_user_entity_id primary key (prow_user_entity_id, prow_prog_entity_id)
 )
-	
+
+insert into curriculum.program_reviews (prow_user_entity_id,prow_prog_entity_id,prow_review,prow_rating,prow_modified_date)
+values
+(6,1,'Course javascript sangat comprehensif, mudah diikutin, trainer sangat menguasai materi',4,'2023-02-23'),
+(2,2,'Very good, has allowed me to grasp the basic syntax and structure of Golang. Also provides effective challenges.',7,'2023-05-30'),
+(4,3,'we going to practice variables and strings in JavaScript so you can hone your skills and feel confident taking them to the real world.',5,'2023-03-12')
+
+select * from curriculum.program_reviews
+
 create table curriculum.sections(
 	sect_title varchar(100),
 	sect_description varchar(256),
@@ -70,11 +154,18 @@ create table curriculum.sections(
 	sect_total_minute integer,
 	sect_modified_date timestamp,
 	sect_prog_entity_id integer,
-	sect_id serial,
-	constraint sect_id primary key (sect_id, sect_prog_entity_id)
-	foreign key (sect_prog_entity_id) references curriculum.program_entity (prog_entity_id)	
-	
+	sect_id serial unique,
+	constraint sect_id primary key (sect_id, sect_prog_entity_id),
+	foreign key (sect_prog_entity_id) references curriculum.program_entity (prog_entity_id)		
 )
+
+insert into curriculum.sections (sect_id,sect_prog_entity_id,sect_title,sect_description,sect_total_section,sect_total_lecture,sect_total_minute,sect_modified_date)
+values
+(1,1,'Javascript Fundamental',null,12,5,60,'2022-08-01'),
+(2,1,'Iteration',null,23,8,120,'2022-03-10'),
+(3,1,'Data Structure',null,8,7,80,'2023-01-15')
+
+select * from curriculum.sections
 	
 -- CREATE SEQUENCE sect_id TABLE SECTIONS
 CREATE SEQUENCE seq_sect_id
@@ -89,12 +180,12 @@ create or replace function sectIdSeq()
 end;$$ 
 language sql
 
-alter table curriculum.sections alter column sect_id set default sectIdSeq()
-	
+alter table curriculum.sections alter column sect_id set default sectIdSeq()	
+
 create table curriculum.section_detail(
-	secd_id serial primary key,
+	secd_id serial primary key unique,
 	secd_title varchar(256),
-	secd_preview char(1) check (secd_preview in(0,1)),
+	secd_preview char(1) CHECK (secd_preview in ('0' ,'1')),
 	secd_score integer,
 	secd_note varchar(256),
 	secd_minute integer,
@@ -102,6 +193,13 @@ create table curriculum.section_detail(
 	secd_sect_id integer,
 	foreign key (secd_sect_id) references curriculum.sections (sect_id)	
 )
+
+insert into curriculum.section_detail (secd_id,secd_title,secd_preview,secd_score,secd_note,secd_minute,secd_modified_date)
+values
+(1,'Course Structure And Project',1,7,'you will learning how to create new project',185,'2023-03-23'),
+(2,'Setting up Visual Code',1,9,null,75,'2023-05-15')
+
+select * from curriculum.section_detail
 	
 -- CREATE SEQUENCE secd_id TABLE SECTION DETAIL
 CREATE SEQUENCE seq_secd_id
@@ -119,14 +217,22 @@ language sql
 alter table curriculum.section_detail alter column secd_id set default secdIdSeq()
 	
 create table curriculum.section_detail_material(
-	sedm_id serial primary key,
+	sedm_id serial primary key unique,
 	sedm_filename varchar(55),
 	sedm_filesize integer,
 	sedm_filetype varchar(15),
 	sedm_filelink varchar(255),
 	sedm_modified_date timestamp,
+	sedm_secd_id integer,
 	foreign key (sedm_secd_id) references curriculum.section_detail (secd_id)
 )
+
+insert into curriculum.section_detail_material (sedm_id,sedm_filename,sedm_filesize,sedm_filetype,sedm_filelink,sedm_modified_date,sedm_secd_id)
+values
+(1,'video1.mp4',12345,'MP4','https://codeacademy/assets','2023-01-23',1),
+(2,'video2.mp4',2435,'MP4','https://codeacademy/assets/gol','2023-01-23',2)
+
+select * from curriculum.section_detail_material
 	
 -- CREATE SEQUENCE sedm_id TABLE SECTION DETAIL MATERIAL
 CREATE SEQUENCE seq_sedm_id
@@ -142,3 +248,4 @@ end;$$
 language sql
 
 alter table curriculum.section_detail_material alter column sedm_id set default sedmIdSeq()
+
