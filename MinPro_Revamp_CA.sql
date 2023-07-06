d89ffafcf500e9a7ba506615a15860a0c74265e6
fatah-illah <fatahillahera18@gmail.com>
2023-07-06 15:23:25 +0700

    Insert Data

--- MinPro_Revamp_CA.sql
+++ MinPro_Revamp_CA.sql
@@ -61,7 +61,7 @@ BEGIN
 		END INTO NEW.usac_account_number
 		FROM payment.bank
 		LEFT JOIN payment.fintech ON FALSE
-		WHERE bank.entity_id = NEW.usac_bank_entity_id;
+		WHERE bank.bank_entity_id = NEW.usac_bank_entity_id;
 	END IF;
 	
 	RETURN NEW;
@@ -98,15 +98,15 @@ BEGIN
 		NEW.trpa_code_number := 'TR-' || to_char(current_date, 'YYYYMMDD') || LPAD(NEXTVAL('trpa_code_number_seq')::TEXT, 5, 'O');
 		
 		SELECT usac_account_number INTO NEW.trpa_source_id
-		FROM users_account
+		FROM payment.users_account
 		WHERE usac_user_entity_id = NEW.trpa_user_entity_id;
 		
 		SELECT usac_account_number INTO NEW.trpa_target_id
-		FROM users_account
+		FROM payment.users_account
 		WHERE usac_user_entity_id = NEW.trpa_user_entity_id;
 		
 		SELECT user_entity_id INTO NEW.trpa_user_entity_id
-		FROM users
+		FROM users.users
 		WHERE user_entity_id = NEW.trpa_user_entity_id;
 	END IF;
 	
@@ -118,3 +118,58 @@ CREATE TRIGGER trigger_set_trpa_code_and_ids
 	BEFORE INSERT ON payment.transaction_payment
 	FOR EACH ROW
 	EXECUTE FUNCTION set_trpa_code_number();
+	
+	
+-- 
+-- DATA
+-- 
+
+INSERT INTO users.business_entity (entity_id) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9);
+SELECT * FROM users.business_entity;
+
+INSERT INTO users.users (user_entity_id) VALUES
+(1),
+(2),
+(3),
+(4),
+(5),
+(6),
+(7),
+(8),
+(9);
+SELECT * FROM users.users;
+
+INSERT INTO payment.bank (bank_entity_id, bank_code, bank_name) VALUES
+(6, 'BCA', 'Bank Central Asia'),
+(7, 'BNI', 'Bank Negara Indonesia'),
+(8, 'MAN', 'Bank Mandiri'),
+(9, 'JAGO', 'Bank Jago');
+SELECT * FROM payment.bank;
+
+INSERT INTO payment.fintech (fint_entity_id, fint_code, fint_name) VALUES
+(7, 'GOTO', 'Payment GoTo'),
+(8, 'OVO', 'OVO'),
+(9, 'SP', 'Shopee Paylater');
+SELECT * FROM payment.fintech;
+
+INSERT INTO payment.users_account (usac_bank_entity_id, usac_user_entity_id, usac_account_number, usac_saldo, usac_type, usac_status) VALUES
+(8, 3, 131899555, 100000, 'debit', 'active'),
+(9, 3, 087654321, 50000, 'payment', 'inactive');
+SELECT * FROM payment.users_account;
+
+INSERT INTO payment.transaction_payment (trpa_id, trpa_code_number, trpa_order_number, trpa_debit, trpa_credit, trpa_type, trpa_note, trpa_modified_date, trpa_source_id, trpa_target_id, trpa_user_entity_id) VALUES
+(1, 20220727#000001, null, 0,	50000, 'top-up', 'topup bank ke goto', null, 131899555, 087654321, 3),	
+(2,	20220727#000002, null, 50000,	0, 'top-up', 'receive topup', null, 087654321, 131899555, 3),
+(3,	20220727#000003, null, null, 10000, 'order', 'bayar shoppe order', null, 131899555, 99999, 3);
+SELECT * FROM payment.transaction_payment;
+
+
